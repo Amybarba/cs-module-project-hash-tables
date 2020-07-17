5f9b2b30e22d1e99c50ab0d13691bc552f20701d
Amy Barba <Amybarba70@gmail.com>
2020-07-16 23:04:59 -0700

    base hashtable working and passing tests

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -2,6 +2,7 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -22,7 +23,11 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
-
+        self.capacity = capacity
+        self.storage = [None] * self.capacity
+        self.size = 0
+        self.max_load_factor = 0.7
+        self.min_load_factor = 0.2
 
     def get_num_slots(self):
         """
@@ -36,6 +41,7 @@ class HashTable:
         """
         # Your code here
 
+        return len(self.storage)
 
     def get_load_factor(self):
         """
@@ -44,17 +50,45 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        return self.size / self.capacity if self.size > 0 else 0
 
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
 
         Implement this, and/or DJB2.
-        """
 
         # Your code here
+        The FNV_offset_basis is the 64-bit FNV offset basis
+        value: 14695981039346656037 (in hex, 0xcbf29ce484222325).
+        The FNV_prime is the 64-bit FNV prime value:
+        1099511628211 (in hex, 0x100000001b3).None
 
+        Algorithm:
+        hash := FNV_offset_basis do
+
+        for each byte_of_data to be hashed
+        hash := hash x FNV_prime
+        hash := hash XOR byte_of_data
+
+        return hash
+
+        Why bitwise?
+        - Scramble thoroughly!
+        - Use every bit that we are given!
+
+
+        FNV_offset_basis = 14695981039346656037
+        FNV_prime = 1099511628211
+
+        byte_keys = key.encode()
+
+        for byte in byte_keys:
+            hashed_key = hashed_key * FNV_prime
+            hashed_key = hashed_key ^ byte
+
+        return hashed_key
+        """
 
     def djb2(self, key):
         """
@@ -64,13 +98,17 @@ class HashTable:
         """
         # Your code here
 
+        hash = 5381
+        for l in key:
+            hash = (hash * 33) + ord(l)
+        return hash
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
+        # return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -82,7 +120,30 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        """
+        if self.get_load_factor() > .7:
+            self.resize(self.capacity * 2)
+            """
+        # hash the key, to get the index
+        index = self.hash_index(key)
+        self.size += 1
+        # check the index, if it's empty put a node there
+        if self.storage[index] is None:
+            self.storage[index] = HashTableEntry(key, value)
+        # otherwise, iterate through the linked list
+        else:
+            current_node = self.storage[index]
+            while current_node:
+                # Check for the key, update value if it's there
+                if current_node.key == key:
+                    current_node.value = value
+                    return
+                # if we reach the end add a new node
+                elif current_node.next:
+                    current_node = current_node.next
+                else:
+                    current_node.next = HashTableEntry(key, value)
+                    return
 
     def delete(self, key):
         """
@@ -94,6 +155,27 @@ class HashTable:
         """
         # Your code here
 
+        index = self.hash_index(key)
+        # if there is a None at the index
+        if self.storage[index]:
+
+            self.size -= 1
+
+            if self.storage[index].key == key:
+                if self.storage[index].next is not None:
+                    self.storage[index] = self.storage[index].next
+                else:
+                    self.storage[index] = None
+            else:
+                current_node = self.storage[index]
+                while current_node.next:
+                    # If the target node is the head
+                    if current_node.next.key == key:
+                        current_node.next = None
+                    else:
+                        current_node = current_node.next
+        else:
+            print("WARNING: sound the alarm, key not found")
 
     def get(self, key):
         """
@@ -105,6 +187,19 @@ class HashTable:
         """
         # Your code here
 
+        index = self.hash_index(key)
+
+        # if there's nothing at the index
+        if self.storage[index]:
+            current_node = self.storage[index]
+            while current_node:
+                if current_node.key == key:
+                    return current_node.value
+                else:
+                    current_node = current_node.next
+        else:
+            return self.storage[index]
+
 
     def resize(self, new_capacity):
         """
@@ -115,6 +210,33 @@ class HashTable:
         """
         # Your code here
 
+        # save our old storage
+        if self.get_load_factor() > self.max_load_factor:
+            old_storage = self.storage.copy()
+        # make a new, bigger storage!
+            self.capacity = new_capacity or self.capacity * 2
+            self.storage = [None] * self.capacity
+
+        # iterate through our hashlist
+            for item in old_storage:
+            ## Iterate through every linked list
+                while item:
+                ## re-insert key, value
+                    self.put(item.key, item.value)
+                    item = item.next
+            return
+        elif self.get_load_factor() < self.min_load_factor and self.capacity > 8:
+            old_storage = self.storage.copy()
+            self.capacity = new_capacity or self.capacity / 2
+            self.storage = [None] * self.capacity
+
+            for item in old_storage:
+                while item:
+                    self.put(item.key, item.value)
+                    item = item.next
+            return
+        else:
+            return
 
 
 if __name__ == "__main__":
